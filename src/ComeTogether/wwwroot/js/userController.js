f3ee2f73fc3d9a62615f5eb2bac35390149b062d
f1tzgerald <alter.vetal@mail.ru>
2016-04-04 18:09:17 +0300

    get all users js

---
+++ src/ComeTogether/wwwroot/js/userController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("tasks_app").controller("userController", userController);
+
+    function userController ($http) {
+
+        var vm = this;
+        vm.users = [];
+
+        // Get All Users
+        $http.get("/api/users")
+            .then(function (response) {
+                //Success
+                vm.users = response.data.name;
+            }, function (error) {
+                //Fail
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () { });
+    }
+})();
