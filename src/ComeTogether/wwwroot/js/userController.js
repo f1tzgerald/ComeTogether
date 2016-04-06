c7503c3a38f5b8cb018e70e8f0c1d80b66bc87af
f1tzgerald <alter.vetal@mail.ru>
2016-04-06 13:27:22 +0300

    add js user controller

--- src/ComeTogether/wwwroot/js/userController.js
+++ src/ComeTogether/wwwroot/js/userController.js
@@ -7,17 +7,54 @@
     function userController ($http) {
 
         var vm = this;
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        // CHANGE THIS COUNT TO 25
+        vm.selectorCount = 2; //25 always
+        vm.skipCount = 0;
+
         vm.users = [];
 
-        // Get All Users
-        $http.get("/api/users")
+        // Get All Users (TOP 25)
+        $http.get("/api/users/" + vm.selectorCount + "/" + vm.skipCount)
             .then(function (response) {
                 //Success
-                vm.users = response.data.name;
+                vm.users = response.data;
+                vm.skipCount += vm.selectorCount;
+                console.log(vm.users);
             }, function (error) {
                 //Fail
                 vm.errorMessage = "Failed to load data: " + error;
             })
-            .finally(function () { });
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+        console.log(vm.users);
+
+        // Get more users (+25)
+        vm.ShowMoreUsers = function () {
+            vm.isBusy = true;
+            vm.newusers = {};
+
+            $http.get("/api/users/" + vm.selectorCount + "/" + vm.skipCount)
+                .then(function (response) {
+                    //success
+                    vm.skipCount += vm.selectorCount;
+                    vm.newusers = response.data;
+                    console.log(vm.newusers);
+                    console.log(response.data.length);
+
+                    for (var i = 0; i < response.data.length; i++) {
+                        vm.users.push(vm.newusers[i]);
+                    }
+
+                }, function (error) {
+                    //error
+                }).finally(function () {
+                    vm.isBusy = false;
+                });
+        }
     }
 })();
